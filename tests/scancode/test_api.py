cc48c54ea267c2d902b69d2119d1b8d1512d4b52
Jillian <jdaguil@nexb.com>
2016-03-03 16:33:11 -0800

    #214: Added the ability to apply a configurable license policy
     * based on a configuration in a .scancode.yml config file
     * the config file is either at a config_location or a default

--- tests/scancode/test_api.py
+++ tests/scancode/test_api.py
@@ -26,6 +26,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+from collections import OrderedDict
 import os
 
 from commoncode.testcase import FileBasedTesting
@@ -56,3 +57,29 @@ class TestAPI(FileBasedTesting):
         info = api.get_file_infos(test_dir, as_list=False)
         is_key_values = [v for k, v in info.items() if k.startswith('is_')]
         assert all(v is not None for v in is_key_values)
+
+    def test_get_license_with_policy_configuration(self):
+        config = self.get_test_loc('api/scancode.yml')
+        test_file = self.get_test_loc('api/LICENSE.txt')
+        result = api.get_licenses(location=test_file, config_location=config)
+        expected = [OrderedDict([
+
+            ('key', 'bsd-new'),
+            ('score', 100.0),
+            ('short_name', 'BSD-Modified'),
+            ('category', 'Permissive'),
+            ('owner', 'Regents of the University of California'),
+            ('homepage_url', 'http://www.opensource.org/licenses/BSD-3-Clause'),
+            ('text_url', 'http://www.opensource.org/licenses/BSD-3-Clause'),
+            ('dejacode_url', 'https://enterprise.dejacode.com/urn/urn:dje:license:bsd-new'),
+            ('spdx_license_key', 'BSD-3-Clause'),
+            ('spdx_url', 'https://spdx.org/licenses/BSD-3-Clause'),
+            ('start_line', 4), ('end_line', 12),
+            ('policy', 'Restricted License'),
+            ('matched_rule', OrderedDict([
+                ('identifier', 'bsd-new_158.RULE'),
+                ('license_choice', False),
+                ('licenses', [u'bsd-new'])])
+                )]
+        )]
+        assert expected == list(result)
